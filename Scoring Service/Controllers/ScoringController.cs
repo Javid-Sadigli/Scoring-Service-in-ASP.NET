f1f1c67a6155da4abb2d689f9f8481251199491e
Javid Sadigli <jsadigli@simbrella.com>
2025-01-28 05:49:52 -0800

    Started an ASP.NET application

---
+++ Scoring Service/Controllers/ScoringController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scoring_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoringController : ControllerBase
+    {
+
+    }
+}
