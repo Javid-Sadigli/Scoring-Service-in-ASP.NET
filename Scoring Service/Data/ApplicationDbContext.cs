fd73e5dfa64071b54b6a7a2621beac0a1acdd0cd
Javid-Sadigli <cavidoffical@gmail.com>
2025-01-28 06:18:22 -0800

    Added Database Connection

---
+++ Scoring Service/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Scoring_Service.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        // Database Sets 
+
+    }
+}
